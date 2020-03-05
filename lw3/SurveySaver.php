4d239d5aa980a3f213d1361230c1e45cf9889423
Daria <erika-vkn@yandex.ru>
2020-03-05 14:08:02 +0300

    3.4 (merging file and new info added)

--- lw3/SurveySaver.php
+++ lw3/SurveySaver.php
@@ -1,8 +1,34 @@
 <?php
+header("Content-Type: text/plain");
 function getQueryStringParameter($name)
 {
     return isset($_GET[$name]) ? $_GET[$name] : null;
 }
+
+function Object2File($obj, $filename)
+{
+    $str_value = serialize($obj);
+    file_put_contents($filename, $str_value);
+}
+
+function ObjectFromFile($filename)
+{
+    $file = file_get_contents($filename);
+    return unserialize($file);
+}
+
+function MergeFileInfo($array1, $array2)
+{
+    foreach ($array1 as $key => $value1)
+    {
+        if ($value1 != null)
+        {
+            $array2[$key] = $value1;
+        }
+    }
+    return $array2;
+}
+
 $dir = "data/";
 if (!file_exists($dir))
 {
@@ -12,13 +38,42 @@ $email = getQueryStringParameter('email');
 $firstName = getQueryStringParameter('first_name');
 $lastName = getQueryStringParameter('last_name');
 $age = getQueryStringParameter('age');
-//if (file_exists(data/$email.txt)) {
-//    fopen("data/$email.txt", r+  )
-//}
 $surveyInfo = [
     'email' => $email,
     'first_name' => $firstName,
     'last_name' => $lastName,
     'age' => $age
 ];
-echo $surveyInfo['last_name'] . $surveyInfo['age'];
+if ($email === null)
+{
+    echo 'Please enter your email address';
+    $emailCheck = false;
+}
+elseif (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $emailCheck = true;
+}
+else {
+    echo 'Email is incorrect';
+    $emailCheck = false;
+}
+if ($emailCheck) {
+    $filename = "data/$email.txt";
+}
+if (file_exists($filename)) {
+    $fileNew = false;
+}
+if ((!file_exists($filename)) && ($emailCheck))
+{
+    fopen($filename, "w");
+    $fileNew = true;
+}
+
+if ($fileNew) {
+    Object2File($surveyInfo, $filename);
+}
+else
+{
+    $infoFromFile = ObjectFromFile($filename);
+    $infoRefreshed = MergeFileInfo($surveyInfo, $infoFromFile);
+    Object2File($infoRefreshed, $filename);
+}
