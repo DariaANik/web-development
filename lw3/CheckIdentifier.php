91da1bc55975e4fabe5661b9382f34333d6f9b92
Daria <erika-vkn@yandex.ru>
2020-03-16 23:45:32 +0300

    3.2

--- lw3/CheckIdentifier.php
+++ lw3/CheckIdentifier.php
@@ -1,23 +1,46 @@
 <?php
-header("Content-Type: text/plain");
-function getQueryStringParameter($name)
+
+function getQueryStringParameter(string $name): ?string
 {
     return isset($_GET[$name]) ? $_GET[$name] : null;
 }
+
+function checkIdent(string $identifier): int
+{
+    if (!ctype_alpha($identifier[0]))
+    {
+        $value = 1;
+    }
+    elseif (!ctype_alnum($identifier))
+    {
+        $value = 2;
+    }
+    else
+    {
+        $value = 3;
+    }
+    return $value;
+}
+header("Content-Type: text/plain");
 $identifier = getQueryStringParameter('identifier');
-if ($identifier === null)
+$value = 0;
+if ($identifier)
 {
-    echo "Not found";
+    $value = checkIdent($identifier);
 }
-elseif (!ctype_alpha($identifier[0]))
+
+switch ($value)
 {
-    echo "Not identifier. The first character is incorrect";
-}
-elseif (!ctype_alnum($identifier))
-{
-    echo "Not identifier. Contains incorrect characters";
-}
-else
-{
-    echo "Identifier";
+    case 0:
+        echo "Identifier not found";
+        break;
+    case 1:
+        echo "Not identifier. The first character is incorrect";
+        break;
+    case 2:
+        echo "Not identifier. Contains incorrect characters";
+        break;
+    case 3:
+        echo "Identifier";
+        break;
 }
